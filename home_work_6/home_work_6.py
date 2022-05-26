8791118e33267019b0af401f3b8bfdb55165fe44
Oleksandr <alexshestalov@gmail.com>
2022-05-26 17:29:26 +0300

    reworked

--- home_work_6/home_work_6.py
+++ home_work_6/home_work_6.py
@@ -10,28 +10,49 @@
 -визначити кількість парних та непарних елементів у послідовності
 """
 
-l =[]
-
+counter = 0
+sum_attempts = 0
+max_attempt = 0
+min_attempt = 100
+odd = 0
+even = 0
 
 while True:
 
+    counter += 1
+
     ask = int(input('Try only integers: '))
 
+    sum_attempts += ask
+
+    # check max integer
+    if ask > max_attempt:
+        max_attempt = ask
+
+    # if ask will be more 100
+    if ask > min_attempt:
+        min_attempt += ask
+
+    # check min integer
+    if ask < min_attempt and ask != 0:
+        min_attempt = ask
+
+    # check even
+    if ask % 2 == 0 and ask != 0:
+        even += 1
+
+    # check odd
+    if ask % 2 == 1:
+        odd += 1
+
     if ask == 0:
-
-        even = len([x for x in l if x % 2 == 0])
-        odd = len([x for x in l if x % 2 == 1])
-
-        print("Attempts:",len(l))
-        print("Sum attempts:",sum(l))
-        print("Arithmetic mean:",sum(l)/len(l))
-        print("Min int:",min(l))
-        print("Max int:",max(l))
-        print("Number of even int:",even)
-        print("Number of odd int:",odd)
+        print("Attempts:", counter-1)
+        print("Sum attempts:", sum_attempts)
+        print("Arithmetic mean:", sum_attempts/(counter-1))
+        print("Min int:", min_attempt)
+        print("Max int:", max_attempt)
+        print("Number of even int:", even)
+        print("Number of odd int:", odd)
         break
-    
-    else:
-        l.append(ask)
 
 
