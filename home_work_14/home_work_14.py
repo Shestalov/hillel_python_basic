f8e56613a8e39a9f8af0431e705b4c19dc30d9ef
Oleksandr <alexshestalov@gmail.com>
2022-06-06 18:23:40 +0300

    square

--- home_work_14/home_work_14.py
+++ home_work_14/home_work_14.py
@@ -1,2 +1,11 @@
-def square(n):
-    pass
+def square(square_side):
+    """
+    Given an int, return the perimeter, area, diagonal of the square.
+
+    :param square_side: int
+    :return: tuple, Perimetr, Area, Diagonal of the square.
+    """
+    return square_side * 4, square_side * square_side, square_side * (2 ** 0.5)
+
+
+print(square(16))
