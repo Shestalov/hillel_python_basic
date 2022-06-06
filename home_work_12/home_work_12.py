b79fdbd96351983b4d5284d5926445964f3b4be3
Oleksandr <alexshestalov@gmail.com>
2022-06-06 11:37:53 +0300

    fixed

--- home_work_12/home_work_12.py
+++ home_work_12/home_work_12.py
@@ -1,6 +1,9 @@
 set_1 = {1, 2, 3, 4}
 set_2 = {1, 2, 3}
 
+# one line
+print(set_2 - set_1 == set())
+
 # only if else
 if set_1 >= set_2:
     print("True")
@@ -18,7 +21,6 @@ def superset(first_set, second_set):
 
 my_func = superset(set_1, set_2)
 print(my_func)
-
 
 # checking (set_1 is a superset for set_2)
 print(set_1.issuperset(set_2))
