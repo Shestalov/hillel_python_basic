5cb15358686ad735630468914e4a9b2702409b53
Oleksandr <alexshestalov@gmail.com>
2022-06-06 16:50:02 +0300

    add docktest

--- home_work_13/home_work_13.py
+++ home_work_13/home_work_13.py
@@ -1,9 +1,16 @@
+import doctest
+
+
 def sum_digits(number):
     """
     Given a positive integer, return the multiplication of all digits by skipping zero.
 
     :param number: int
     :return: int, Multiplication of all digits by skipping zero.
+
+    >>> sum_digits(123405)
+    120
+
     """
     res = 1
     for digit in str(number):
@@ -13,8 +20,10 @@ def sum_digits(number):
     return res
 
 
-result = sum_digits(999)
+result = sum_digits(123405)
 print(result)
 
 if __name__ == '__main__':
     help(sum_digits)
+    doctest.testmod()
+    sum_digits(123405)
