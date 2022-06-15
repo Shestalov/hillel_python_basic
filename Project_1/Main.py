111022e6636296e2492302b7264dc2839868e00f
Oleksandr <alexshestalov@gmail.com>
2022-06-15 21:56:28 +0300

    parse

--- Project_1/Main.py
+++ Project_1/Main.py
@@ -1,8 +1,16 @@
+import argparse
+
 logs = {"qwe": "123"}
 
 
+def parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--username')
+    parser.add_argument('--password')
+    return parser.parse_args()
+
 def decorator(func):
-    def wrapper(name, pas):
+    def wrapper(name: str, pas: str) -> bool:
         if check_password(name, pas):
             return func(name, pas)
         else:
@@ -25,12 +33,22 @@ if __name__ == '__main__':
     counter = 3
     while counter >= 1:
 
-        print(f"You have {counter} attempts.")
-
-        username = input("Username: ")
-        password = input("Password: ")
-
-        log = login(username, password)
+        args = parse()
+        if login(args.username, args.password):
+            log = login(args.username, args.password)
+        elif args.username:
+            print(f"You have {counter} attempts.")
+            password = input("Password: ")
+            log = login(args.username, password)
+        elif args.password:
+            print(f"You have {counter} attempts.")
+            username = input("Username: ")
+            log = login(username, args.password)
+        else:
+            print(f"You have {counter} attempts.")
+            username = input("Username: ")
+            password = input("Password: ")
+            log = login(username, password)
 
         if log is True:
             print("You are in the system!")
