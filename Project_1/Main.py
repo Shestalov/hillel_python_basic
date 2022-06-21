3c1ce8c9b25c6d8b9b1b50aa124394f749e5a2f5
Oleksandr <alexshestalov@gmail.com>
2022-06-21 18:13:17 +0300

    added datetime

--- Project_1/Main.py
+++ Project_1/Main.py
@@ -1,6 +1,7 @@
 import argparse
+import datetime
 
-logs = {"qwe": "123"}
+logs = {"qwe": "123", 'time': datetime.datetime(2022, 6, 21, 18, 10, 0, 153718)}
 
 
 def parse():
@@ -29,34 +30,50 @@ def check_password(name: str, pas: str) -> bool:
     return logs.get(name) == pas
 
 
+def block(last_try_time) -> bool:
+    if datetime.datetime.now() > last_try_time + datetime.timedelta(seconds=60*5):
+        return True
+    else:
+        print("You are blocked! Try after ", (last_try_time + datetime.timedelta(seconds=60*5)).strftime("%H:%M:%S"))
+        return False
+
+
 if __name__ == '__main__':
 
-    counter = 3
-    print(f"You have {counter} attempts.")
+    while True:
 
-    username = parse().username or input("Username: ")
-    password = parse().password or input("Password: ")
-
-    while counter > 1:
-
-        if login(username, password) is True:
-            print("You are in the system!")
+        if not block(logs["time"]):
             break
 
-        elif login(username, password) is False:
-            print("Login or password is WRONG.")
-            counter -= 1
-            print(f"You have {counter} attempts.")
+        else:
+            counter = 4
 
-            if parse().username is None:
-                username = input("Username: ")
+            username = parse().username or input("Username: ")
+            password = parse().password or input("Password: ")
 
-            if parse().password is None:
-                password = input("Password: ")
+            while counter > 1:
+
+                if login(username, password) is True:
+                    print("You are in the system!")
+                    break
+
+                elif login(username, password) is False:
+                    print("Login or password is WRONG.")
+                    counter -= 1
+                    print(f"You have {counter} attempts.")
+
+                    if parse().username is None:
+                        username = input("Username: ")
+
+                    if parse().password is None:
+                        password = input("Password: ")
+
+                    else:
+                        username = input("Username: ")
+                        password = input("Password: ")
 
             else:
-                username = input("Username: ")
-                password = input("Password: ")
+                print("Attempts expired")
+                print("You are blocked for 5 min.")
 
-    else:
-        print("Attempts expired")
+        break
