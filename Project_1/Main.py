c5f6366acccfbb37aceb43ece1f27fdba0ce8a5a
Oleksandr <alexshestalov@gmail.com>
2022-06-27 20:25:33 +0300

    try except

--- Project_1/Main.py
+++ Project_1/Main.py
@@ -1,7 +1,11 @@
 import argparse
 import datetime
 
-logs = {"qwe": "123", 'time': "2022-06-22 16:10:00:997317"}
+logs = {"qwe": "123", 'time': "2022-06-28 19:10:00:997317"}
+
+
+class UserDoesNotExist(Exception):
+    pass
 
 
 def parse():
@@ -12,52 +16,58 @@ def parse():
 
 
 def decorator(func):
-    def wrapper(user_name: str, user_password: str, now: str, last: str) -> bool:
-        if check_password(user_name, user_password) and not block(now, last):
-            return func(user_name, user_password, now, last)
-        else:
+    def wrapper(user_name: str, user_password: str, time_now: str) -> bool:
+
+        try:
+            if not check_password(user_name, user_password):
+                print("Wrong password")
+                return False
+        except UserDoesNotExist as a:
+            print(a)
             return False
+
+        if not is_block(time_now):
+            print("Wrong time")
+            return False
+
+        return func(user_name, user_password, time_now)
 
     return wrapper
 
 
 @decorator
-def login(user_name: str, user_password: str, now: str, last: str) -> bool:
+def login(user_name: str, user_password: str, time_now: str) -> bool:
     return True
 
 
 def check_password(user_name: str, user_password: str) -> bool:
-    return logs.get(user_name) == user_password
-
-
-def block(now: str, last: str) -> bool:
-    if datetime.datetime.strptime(now, "%Y-%m-%d %H:%M:%S:%f") > \
-            (datetime.datetime.strptime(last, "%Y-%m-%d %H:%M:%S:%f") + datetime.timedelta(seconds=60 * 5)):
-        return False
+    if user_name not in logs:
+        raise UserDoesNotExist("Wrong User Name")
     else:
-        print("You are blocked! Try after ",
-              datetime.datetime.strptime(last, "%Y-%m-%d %H:%M:%S:%f") + datetime.timedelta(seconds=60 * 5))
-        return True
+        return logs.get(user_name) == user_password
+
+
+def is_block(time_now: str) -> bool:
+    last_time = logs["time"]
+    return last_time > time_now
 
 
 if __name__ == '__main__':
 
     now_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S:%f")
-    last_time = logs["time"]
 
     counter = 4
 
     username = parse().username or input("Username: ")
     password = parse().password or input("Password: ")
 
-    while counter > 1:
+    while counter >= 1:
 
-        if login(username, password, now_time, last_time) is True:
+        if login(username, password, now_time) is True:
             print("You are in the system!")
             break
 
-        elif login(username, password, now_time, last_time) is False:
-            print("Login or password is WRONG.")
+        else:
             counter -= 1
             print(f"You have {counter} attempts.")
 
