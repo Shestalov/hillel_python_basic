7777aa75a857f79a45164f408f27a729ade5a040
Oleksandr <alexshestalov@gmail.com>
2022-07-03 19:52:36 +0300

    with open

--- test/test.py
+++ test/test.py
@@ -1,7 +1,6 @@
 import argparse
 import datetime
-
-logs = {"qwe": "123", 'time': "2022-06-27 19:10:00:997317"}
+import json
 
 
 class UserDoesNotExist(Exception):
@@ -16,43 +15,60 @@ def parse():
 
 
 def decorator(func):
-    def wrapper(user_name: str, user_password: str, time_now: str) -> bool:
+    def wrapper(user_name: str, user_password: str, time_now: str, log: dict) -> bool:
 
         try:
-            if not check_password(user_name, user_password):
+            if not check_password(user_name, user_password, log):
                 print("Wrong password")
                 return False
         except UserDoesNotExist as a:
             print(a)
             return False
 
-        if not is_block(time_now):
-            print("Wrong time")
+        if not is_block(time_now, log):
+            print("YOU are blocked, try after: ", log["time"])
             return False
 
-        return func(user_name, user_password, time_now)
+        return func(user_name, user_password, time_now, log)
 
     return wrapper
 
 
 @decorator
-def login(user_name: str, user_password: str, time_now: str) -> bool:
+def login(user_name: str, user_password: str, time_now: str, log: dict) -> bool:
     return True
 
 
-def check_password(user_name: str, user_password: str) -> bool:
-    if user_name not in logs:
+def check_password(user_name: str, user_password: str, log: dict) -> bool:
+    if user_name not in log:
         raise UserDoesNotExist("Wrong User Name")
     else:
-        return logs.get(user_name) == user_password
+        return log.get(user_name) == user_password
 
 
-def is_block(time_now: str) -> bool:
-    last_time = logs["time"]
-    return last_time > time_now
+def is_block(time_now: str, log: dict) -> bool:
+    return time_now > log["time"]
+
+
+def read_logs(file_name: str) -> dict:
+    try:
+        with open(file_name) as f:
+            return json.load(f)
+    except OSError as e:
+        ...
+
+
+def rewrite_logs(file_name: str, data: dict):
+    try:
+        with open(file_name, 'w') as f:
+            json.dump(data, f)
+    except OSError as e:
+        ...
 
 
 if __name__ == '__main__':
+
+    logs = read_logs("logs.json")
 
     now_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S:%f")
 
@@ -61,14 +77,18 @@ if __name__ == '__main__':
     username = parse().username or input("Username: ")
     password = parse().password or input("Password: ")
 
-    while counter > 1:
+    while counter >= 1:
 
-        if login(username, password, now_time) is True:
+        counter -= 1
+
+        if login(username, password, now_time, logs) is True:
             print("You are in the system!")
             break
 
-        elif login(username, password, now_time) is False:
-            counter -= 1
+        if counter < 1:
+            continue
+
+        else:
             print(f"You have {counter} attempts.")
 
             if parse().username is None:
@@ -77,10 +97,10 @@ if __name__ == '__main__':
             if parse().password is None:
                 password = input("Password: ")
 
-            else:
-                username = input("Username: ")
-                password = input("Password: ")
-
     else:
+        logs["time"] = (datetime.datetime.now() + datetime.timedelta(seconds=60 * 5)).strftime("%Y-%m-%d %H:%M:%S:%f")
+        next_time = logs["time"]
+        rewrite_logs("logs.json", logs)
+
         print("Attempts expired")
-        print("You are blocked for 5 min.")
+        print("You are blocked for 5 min. Try after", logs["time"])
